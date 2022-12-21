878cc027f4d3efff168335ce4b1f93de1c0277c3
MonicaDeBona <debona.monica@outlook.it>
2022-12-21 16:12:12 +0100

    Push email in emailList and print them in html

--- js/main.js
+++ js/main.js
@@ -21,7 +21,8 @@ createApp({
             for (let i = 0; i < 10; i++) {
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
                 .then((response) => {
-                    console.log(response.data)
+                    console.log(response.data.response);
+                    this.emailList.push(response.data.response)
                 })
             }
         }
